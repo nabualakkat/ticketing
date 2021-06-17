99ea10260af51a2cf1c0898d1e0e818a84411ddb
Nabu <alakkat.n@gmail.com>
2021-06-17 09:53:10 -0400

    init commit

---
+++ client/pages/_app.js
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      {currentUser ? 'sign out' : 'sign in'}
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+AppComponent.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
+};
+
+export default AppComponent;
---
+++ client/pages/index.js
@@ -0,0 +1,15 @@
+import buildClient from '../api/build-client';
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return (
+    <h1>{currentUser ? 'You are signed in!' : 'You are NOT signed in'}</h1>
+  );
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
+};
+
+export default LandingPage;
