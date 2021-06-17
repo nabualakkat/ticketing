ed5a0bc9643050af6e3bffe092da83c999e2331f
Nabu <alakkat.n@gmail.com>
2021-06-17 17:18:39 -0400

    moved errors and middlewares to common module

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,9 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
-import { validateRequest } from '../middlewares/validate-request';
 import { Password } from '../services/password';
-import { BadRequestError } from '../errors/bad-request-error';
+import { BadRequestError, validateRequest } from '@natictrader/common';
 import { User } from '../models/user';
 
 const router = express.Router();
